0ac8e96b197374adc14627861dd9c9a097c20f51
Wesley Silva C <wesleysilvaconceicao@outlook.com>
2022-12-02 19:42:31 -0300

    inicio da edição do main

--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -2,9 +2,17 @@
 import './main.sass'
 
 const Main = () => {
+  const imagens = {
+    programacao: '../public/img.programação.png'
+  }
   return (
     <div className="main">
-      
+      <img src={imagens.programacao} alt="tela de um computador em 3d apresentando várias ferramentas tecnologicas." />
+      <p>
+        <span>Onde podemos encontrar a Progração? </span><br />
+        A programação está diretamente realcionada com outros tipos de ferramentas.
+        Armazenamento em nuvem, banco de dados, folhas de estilo e claro lógica de progração, que é responsável por fazer acontercer tudo o que tem de especial em uma página web.
+      </p>
     </div>
   )
 }
