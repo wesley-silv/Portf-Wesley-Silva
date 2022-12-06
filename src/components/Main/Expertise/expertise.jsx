c45d013dae2bd371a90d024aad0c7df606c3d53c
Wesley Silva C <wesleysilvaconceicao@outlook.com>
2022-12-05 22:21:44 -0300

    add expertise the img of node and text with your description

--- src/components/Main/Expertise/expertise.jsx
+++ src/components/Main/Expertise/expertise.jsx
@@ -4,7 +4,8 @@ import './expertise.sass'
 const Expertise = () => {
   const imagens = {
     javascript: '../imagens/img.javascript.png',
-    react: '../imagens/img.react.jpg'
+    react: '../imagens/img.react.jpg',
+    node: '../imagens/img.node.jpg',
   }
   return (
     <div className="expertise">
@@ -19,6 +20,10 @@ const Expertise = () => {
         <img src={imagens.react} alt="logo do react js" />
         O React é uma biblioteca da linguagem JavaScript, ela foi criada pelo Facebook no ano de 2011, e desde então tem dominado o mercado tecnológico na construção de interfaces dos mais diversos tipos. 
         A sua estrutura baseada em componentes faz com que a manutenção e reutilização do código seja facilmente implementada.
+      </div>
+      <div className="expertise-node">
+        O Node js é um ambiente que permite o desenvolvimento com a linguagem JavaScript fora do navegador. O mesmo é muito utilizado na criação do próprio servidor da aplicação, consumo de APIs, conversão de dados através dos objetos JSON, "objeto de notação JavaScript", dentre outras funcionalidades. 
+        <img src={imagens.node} alt="logo do node js" />
       </div>
     </div>
   )
